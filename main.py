8a74d9c880dc35ee0a39b8c619bfeffdd22903af
Vincent Hachin <48289861+VincentHch@users.noreply.github.com>
2020-11-29 09:54:00 +0100

    format file (#2)
    * format file
    
    * New backup
    
    Co-authored-by: Vincent Hachin <vincent.hachin@grenoble-inp.org>
    Co-authored-by: bucky <buckyroberts@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 from datetime import datetime
 
+from thenewboston.constants.network import MAX_POINT_VALUE
 from thenewboston.utils.network import fetch
 
 from config.settings import ACCOUNT_BACKUPS_DIR
 from utils.files import write_json
+from utils.format_results import format_results
 
 PRIMARY_VALIDATOR_IP = '157.230.75.212'
 
@@ -37,7 +39,26 @@ def run():
     now = datetime.now()
     date_time = now.strftime('%Y-%m-%d-%H:%M:%S')
     file_path = os.path.join(ACCOUNT_BACKUPS_DIR, f'{date_time}.json')
-    write_json(file=file_path, data=fetch_account_data())
+    data = format_results(fetch_account_data())
+    verify_results(data=data)
+
+    write_json(
+        file=file_path,
+        data=data
+    )
+
+
+def verify_results(*, data):
+    """
+    Ensure total coins is equal to
+    """
+
+    total = sum(v['balance'] for k, v in data.items())
+
+    if total == MAX_POINT_VALUE:
+        print('\nValid')
+    else:
+        print('\nInvalid')
 
 
 if __name__ == '__main__':
