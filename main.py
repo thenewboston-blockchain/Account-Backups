ef32a811a9b5b563a69acdc7c16ab17c9a8f500e
Bucky Roberts <buckyroberts@gmail.com>
2021-08-26 11:12:18 -0400

    New backup

--- main.py
+++ main.py
@@ -16,7 +16,10 @@ class NetworkException(Exception):
 
 
 def fetch(*, url, headers):
-    """Send a GET request and return response as Python object"""
+    """
+    Send a GET request and return response as Python object
+    """
+
     response = requests.get(url, headers=headers)
     return validate_response(response)
 
@@ -64,6 +67,7 @@ def validate_response(response):
     Validate status code
     Return response as Python object
     """
+
     if response.status_code >= 400:
         err = f'status_code:{response.status_code} - {response.text}'
         raise NetworkException(err)
